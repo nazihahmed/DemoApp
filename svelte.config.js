366825e261734429335301c8931535968406814f
Nazih Ahmed <nazih.dev@gmail.com>
2024-08-22 18:03:10 +0300

    cloudflare support

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	preprocess: [vitePreprocess()],
-	
+
 	kit: {
 		adapter: adapter()
 	}
